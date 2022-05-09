0292d5df0449dd9cc33d2bef00aca51d7179761c
Oleksandr Mashkin <oleksandr.mashkin@gmail.com>
2022-05-09 16:13:47 +0300

    some restructirazation

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
-<html>
- <head>
-  <title>Тестируем PHP</title>
- </head>
- <body>
- <?php echo '<p>Привет, Одуванчик!</p>'; ?>
- </body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Dandelion</title>
+</head>
+<body>
+  <h1> Hello Mash King</h1>
+  <?php echo "hello Mash King"; ?>
+  <?php echo '<p>Привет, Одуванчик!</p>'; ?>
+</body>
 </html>
