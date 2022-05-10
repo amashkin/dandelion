de8e3ec64db729af7acb6c549623fcf22c26c22a
Oleksandr Mashkin <oleksandr.mashkin@gmail.com>
2022-05-10 16:35:29 +0300

    f

--- index.php
+++ index.php
@@ -5,17 +5,11 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Dandelion</title>
-
-  <title>Running a Python script</title>
-        <?PHP
-        echo shell_exec("python3 pump.py");
-        ?>
-
-
 </head>
 <body>
 
 
+<input type="button" id='script' name="scriptbutton" value=" Run Script " onclick="shell_exec('python3 /var/www/html/dandelion/pump.py');">
 
 
 </body>
